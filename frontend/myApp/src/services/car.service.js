65f7113a027f0d0f233051d5f173788713ceee49
Voidzero-development <jhompesc@avans.nl>
2021-06-25 13:08:47 +0200

    Create car.service.js

---
+++ frontend/myApp/src/services/car.service.js
@@ -0,0 +1,22 @@
+import ApiService from "./api.service";
+
+const CarService = {
+    async getAll() {
+        return await ApiService.get('/API/car');
+    },
+    async getById(id) {
+        return await ApiService.get(`/API/car/${id}`);
+    }, 
+    async create(data) {
+        return await ApiService.post('/API/car/', data);
+
+    },
+    async update(id, data) {
+        return await ApiService.patch(`/API/car/${id}`, data)
+    },
+    async delete(id) {
+        return await ApiService.delete(`/API/car/${id}`)
+    }
+}
+
+export { RideService };
